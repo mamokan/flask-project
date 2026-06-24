d6ad278a2eec758d288bdeac809acf32c735489f
mamokan <kanit.almaamoun@gmail.com>
2026-06-24 18:47:27 +0100

    First flask project

---
+++ app/app.py
@@ -0,0 +1,43 @@
+import pickle
+import numpy as np
+from flask import Flask,request, jsonify
+# Initialize the Flask application instance
+app = Flask(__name__)   
+# Load the pre-trained machine learning model into memory
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return "Hello from Flask! 🚀"
+
+@app.route('/health')
+def health():
+    return "OK"
+
+@app.route('/test')
+def test():
+    return "It looks cool!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Extract the JSON payload array sent by the client
+        data = request.get_json()
+        
+        # Convert inputs into a 2D NumPy array structure required by scikit-learn
+        prediction_inputs = np.array(data['features']).reshape(1, -1)
+        
+        # Execute model inference
+        prediction = model.predict(prediction_inputs)
+        
+        # Return the resulting numerical target mapping as a JSON response
+        return jsonify({'prediction': int(prediction[0])})
+        
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    # Start the Flask local development server
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
---
+++ app/train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import  accuracy_score, f1_score
+from sklearn.linear_model import LogisticRegression 
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+df = pd.read_csv('student-lifestyle-and-stress-dataset.csv')
+
+df["Student_Type"] = df["Student_Type"].map({"school": 0, "college": 1,'working_student': 2})
+df = df.dropna()
+
+
+#🤖 Model Training
+X = df.drop("Stress_Level", axis=1)
+y = df["Stress_Level"]
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state= 70)
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+classifier =RandomForestClassifier()
+classifier.fit(x_train_scaled, y_train)
+y_pred_log = classifier.predict(x_test_scaled)
+with open('model.pkl', 'wb') as file:
+    pickle.dump(classifier, file)
+
+print("Model successfully trained and saved as model.pkl")
