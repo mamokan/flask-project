bd811ba12cdecd8041228bb607b79bb54dacfcf6
mamokan <kanit.almaamoun@gmail.com>
2026-06-25 11:05:31 +0100

    Test 0001

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.10-slim
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-WORKDIR /app
 EXPOSE 5000
 CMD python train.py && gunicorn --bind 0.0.0.0:5000 app:app
 
--- train.py
+++ train.py
@@ -10,12 +10,11 @@ df = pd.read_csv('student-lifestyle-and-stress-dataset.csv')
 df["Student_Type"] = df["Student_Type"].map({"school": 0, "college": 1,'working_student': 2})
 df = df.dropna()
 
-print(df)
 
 #🤖 Model Training
 X = df.drop("Stress_Level", axis=1)
 y = df["Stress_Level"]
-"""
+
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 70)
 scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(x_train)
@@ -27,4 +26,3 @@ with open('model.pkl', 'wb') as file:
     pickle.dump(classifier, file)
 
 print("Model successfully trained and saved as model.pkl")
-"""
